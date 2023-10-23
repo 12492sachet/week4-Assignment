1d421e0bb24ad9654d4c3ea27478a14442f1ed5b
lewis machabe <lewismachabe@gmail.com>
2023-10-23 14:34:26 +0300

    function that calculates the area of rectangle

---
+++ functions.js
@@ -0,0 +1,12 @@
+// This function calculates the area of a rectangle
+function calculateArea(length, width) {
+  let area = length * width; // Multiply the length and width to get the area
+  return area; // Return the calculated area
+}
+
+let length = 5; // Length of the rectangle
+let width = 8; // Width of the rectangle
+let area = calculateArea(length, width); // Calling the function to calculate the area
+
+// Displaying the calculated area to the console
+console.log("The area of the rectangle is: " + area);
